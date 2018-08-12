966e344d775bb4af2f94f09ab720550b2c4176be
JACK <jkear@seas.upenn.edu>
2018-08-11 23:28:49 -0400

    hey making progress

---
+++ node-app/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	console.log('light up stuff');
+	res.send({message:'OK'});
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
